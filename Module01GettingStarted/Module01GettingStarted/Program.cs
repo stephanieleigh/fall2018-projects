a837aca9fbedf52473bbf5ff4e476682c6bb2b77
stephanieleigh <36084964+stephanieleigh@users.noreply.github.com>
2018-08-26 17:16:10 -0500

    hello world app

---
+++ Module01GettingStarted/Module01GettingStarted/Program.cs
@@ -0,0 +1,22 @@
+﻿// Stephanie Wilson
+// Advanced C#
+// August 23, 2018
+// Hello World app
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module01GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
