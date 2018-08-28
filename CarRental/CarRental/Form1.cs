006cf1a91c1e2e4d8cfdd01fb1f320ea8bcd7cf4
stephanieleigh <36084964+stephanieleigh@users.noreply.github.com>
2018-08-28 15:59:39 -0500

    Added name, date, etc.

--- CarRental/CarRental/Form1.cs
+++ CarRental/CarRental/Form1.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿// Car Rental Program
+// Advanced C#
+// Written by Stephanie Wilson
+// August 27, 2018
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
