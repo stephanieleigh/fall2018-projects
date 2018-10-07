3472f0c8f42597344d93382d8f7cd4dc18591e67
stephanieleigh <36084964+stephanieleigh@users.noreply.github.com>
2018-10-07 17:00:16 -0500

    add TimerExample to repo

---
+++ TimerExample2/TimerExample2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Advanced C#
+// Module 07
+// Timer Example from MVA tutorial
+// Stephanie Wilson
+// October 5, 2018
+
+using System;
+using static System.Console;
+using System.Timers;
+
+namespace TimerExample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1;
+
+            myTimer.Start();
+
+            WriteLine("Press enter to remove the red event.");
+
+            ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            ForegroundColor = ConsoleColor.Red;
+            WriteLine("Elapsed1: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            ForegroundColor = ConsoleColor.White;
+            WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+    }
+}
