e25fba94ad7d406e0b646238cfc8ebd9bb8d31fa
stephanieleigh <36084964+stephanieleigh@users.noreply.github.com>
2018-10-07 17:11:15 -0500

    Add WinFormsEvents example to repo

---
+++ WinFormEvents/WinFormEvents/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WinFormEvents
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.myLabel = new System.Windows.Forms.Label();
+            this.myOtherLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "CLICK ME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // myLabel
+            // 
+            this.myLabel.AutoSize = true;
+            this.myLabel.Location = new System.Drawing.Point(60, 133);
+            this.myLabel.Name = "myLabel";
+            this.myLabel.Size = new System.Drawing.Size(0, 17);
+            this.myLabel.TabIndex = 1;
+            // 
+            // myOtherLabel
+            // 
+            this.myOtherLabel.AutoSize = true;
+            this.myOtherLabel.Location = new System.Drawing.Point(60, 194);
+            this.myOtherLabel.Name = "myOtherLabel";
+            this.myOtherLabel.Size = new System.Drawing.Size(0, 17);
+            this.myOtherLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 449);
+            this.Controls.Add(this.myOtherLabel);
+            this.Controls.Add(this.myLabel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label myLabel;
+        private System.Windows.Forms.Label myOtherLabel;
+    }
+}
+
---
+++ WinFormEvents/WinFormEvents/Form1.cs
@@ -0,0 +1,38 @@
+﻿// Advanced C#
+// Module 07
+// Example of Windows Forms Events
+// Stephanie Wilson
+// October 5, 2018
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button_MyOtherClick;
+        }
+
+        private void button_MyOtherClick(object sender, EventArgs e)
+        {
+            myOtherLabel.Text = "Hello again!";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            myLabel.Text = "Hello world!";
+        }
+
+    }
+}
